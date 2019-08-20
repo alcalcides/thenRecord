2b69e7368d8ab1d21c5f0a78766022867ae8e748
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-08-20 01:40:36 -0300

    [site]Show git sheet

--- site/view/content/git.php
+++ site/view/content/git.php
@@ -1,5 +1,33 @@
-<?php
-    $file = "./subjects/git/sheet/git-sheet.txt"
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>git | hWorldSheet</title>
+</head>
+<body>
+    
+    <?php
+        $sheetFile = "../../../subjects/git/sheet/git-sheet.txt";
+        $sheetLines = file($sheetFile);
+        $obsFile = "../../../subjects/git/sheet/comments/git-pt-br.txt";
+        $obsLines = file($obsFile);
 
-
-?>
+        if(count($sheetLines) != count($obsLines)){
+            echo "Erro: número de linhas diferentes entre sheet e comment";
+        }
+        else{
+            $numberOfLines = count($obsLines);
+            for ($i = 0; $i < $numberOfLines; $i++) { 
+                $setLine = "";
+                $setLine .= ($i + 1) . "&nbsp;&nbsp;&nbsp;";
+                $setLine .= htmlspecialchars($sheetLines[$i]) . "&nbsp;&nbsp;&nbsp;";
+                if(strlen($obsLines[$i]) > 1) $setLine .= "//";
+                $setLine .= htmlspecialchars($obsLines[$i]);
+                echo "<p>" . $setLine . "</p>";
+            }
+        }
+    ?>
+</body>
+</html>
--- site/view/home.php
+++ site/view/home.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-	<head>
-		<meta charset="UTF-8">
-		<title>Home | hWorldSheet</title>
-	</head>
-	<body>
-		<header>
-			<h1>hWorldSheet</h1>
-			<?php include 'includes/menu.php';?>
-		</header>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Home | hWorldSheet</title>
+</head>
+<body>
+	<header>
+		<h1>hWorldSheet</h1>
+		<?php include 'includes/menu.php';?>
+	</header>
 
-		<?php include 'includes/footer.php';?>
-		
-	</body>
+	<?php include 'includes/footer.php';?>
+	
+</body>
 </html>
