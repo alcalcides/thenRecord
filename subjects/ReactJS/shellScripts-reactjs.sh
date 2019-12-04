2714464e51a8794e9cc1cb428061e2da8b261b65
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2019-12-04 15:03:00 -0300

    [ReactJS]Hello World

---
+++ subjects/ReactJS/shellScripts-reactjs.sh
@@ -0,0 +1,4 @@
+npm install -g create-react-app #instala globalmente o criador de apps Reacts
+create-react-app meuApp #cria estrutura de arquivos para o projeto meuApp (com suporte a transpilações)
+npm run start #start é o script definido em package.json/scripts/start: react-scripts start
+npm start #idem
